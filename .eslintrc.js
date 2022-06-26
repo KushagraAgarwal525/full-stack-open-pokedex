21ba56f4a1c067f2ef066874d5e11f36a9d2f55d
Kushagra Agarwal <kushagraagarwal525.ka@gmail.com>
2022-06-26 18:12:16 +0300

    Add steps for e2e in pipeline

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     "node": true,
     "es6": true,
     "jest/globals": true,
-    "commonjs": true
+    "commonjs": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -18,7 +19,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
